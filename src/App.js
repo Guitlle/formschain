4d00a63365c787c2faf785a6fe7191b591f41daa
Guillermo A <lgar89@gmail.com>
2021-03-15 12:43:01 -0600

    basic functionality of form creation using json schemas and react library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
+import React from 'react';
+import Form from "@rjsf/material-ui";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let schema = {
+  "title": "Create a Form",
+  "type": "object",
+  "description": "This tool lets you define a form. Edit your form below and hit submit when ready. This will store the form specification in your Arweave profile and generate a share link so others can fill the form and send you their response.",
+  "required": ["title"],
+  "properties": {
+    "title": {"type": "string", "title": "Form title", "default": ""},
+    "description": {"type": "string", "title": "Form description", "default": ""},
+    "sections": {
+      "type": "array",
+      "title": "Sections",
+      "items": {
+        "type": "object",
+        "required": [
+          "title"
+        ],
+        "properties": {
+          "title": {
+            "type": "string",
+            "title": "Section Title"
+          },
+          "fields": {
+            "type": "array",
+            "title": "Fields for this section",
+            "items": {
+              "type": "object",
+              "anyOf": [
+                {
+                  "title": "Text field",
+                  "properties": {
+                    "title": {
+                      "type": "string",
+                      "title": "Field name",
+                    },
+                    "placeholder": {
+                      "type": "string",
+                    },
+                    "type" : {
+                      "type": "string", "default": "string"
+                    }
+                  }
+                },
+                {
+                  "title": "Number field",
+                  "properties": {
+                    "title": {
+                      "type": "string",
+                      "title": "Field name",
+                    },
+                    "placeholder": {
+                      "type": "string",
+                    },
+                    "type" : {
+                      "type": "string", "default": "number"
+                    }
+                  }
+                },
+                {
+                  "title": "Date field",
+                  "properties": {
+                    "title": {
+                      "type": "string",
+                      "title": "Field name",
+                    },
+                    "placeholder": {
+                      "type": "string",
+                    },
+                    "type" : {
+                      "type": "string", "default": "string"
+                    },
+                    "format" : {
+                      "type": "string", "default": "date"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+let uiSchema = {
+  title: {
+    
+  },
+  description: {
+      "ui:widget": "textarea",
+      "ui:options": {
+        "rows": 5
+      }
+  },
+  "sections": {
+    "items": {
+      "fields": {
+        "items": {
+          "type": {
+            "ui:widget": "hidden"
+          },
+          "format": {
+            "ui:widget": "hidden"
+          }
+        }
+      }
+    }
+  }
+};
+
+class App extends React.Component {
+  submitData (form) {
+    console.log(form);
+  }
+  
+  errorHandler (form) {
+    console.log(form);
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <Form 
+          schema={schema}
+          uiSchema={uiSchema}
+          onSubmit={this.submitData}
+          onError={this.errorHandler} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
